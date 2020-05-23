61f1acbde073752a3cc98b29cabf9ade580b5a11
Mariane Flor <marianeflor@gmail.com>
2020-05-23 18:30:50 -0300

    images sizes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 export default props => 
+<picture>
     <img 
         src={require(`../assets/imgs/${props.name}.png`)}
         alt={props.name}
         className='image'
     />
+</picture>
